2bcf2f70d2f0947284b020e1ca5ffed93a58d057
Luis Francisco Pérez Hernández <lexius002@gmail.com>
2023-09-07 18:46:37 -0600

    Tile movement implemented

---
+++ Assets/Scripts/SpawnTile.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTile : MonoBehaviour
+{
+    private TilePool _tilePool;
+
+    [SerializeField] private Transform _tilePosition = default;
+    
+    
+    
+    void Start()
+    {
+        _tilePool = TilePool._Instance;
+
+ 
+
+    }
+    
+    void LateUpdate()
+    {
+        _tilePool.GetPooledObject();
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+        GameObject tiles = TilePool._Instance.GetPooledObject();
+
+        if (tiles != null)
+        {
+           // var newPosition = tiles.transform.Find("NextTilePos");
+            tiles.transform.position = new Vector3(0, 0, 125f);
+            tiles.SetActive(true);
+        }
+            
+        }
+    }
+
+        
+}
