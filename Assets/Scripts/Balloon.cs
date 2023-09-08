dc928d9eb3a011a2317aaebb5fd226eae772303e
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2023-09-07 18:26:11 -0600

    add balloonMovement

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -1,14 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Balloon : MonoBehaviour
 {
-    [SerializeField] private AudioClip _popSFX = default; 
+    [SerializeField] private float _speed = default;
+    [SerializeField] private AudioClip _popSFX = default;
+    [SerializeField] private Transform _player = default;
     void Start()
     {
-        
+        _player = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
