48ad4f5eeea26ec93b2f8d9d62b203bbd2e4ab91
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2023-09-15 20:44:28 -0600

    ui time

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -11,16 +11,19 @@ public class UImanager : MonoBehaviour
 
     public void PanelFadeIn()
     {
+        
         _canvasGroup.alpha = 0f;
         _rectTransform.transform.localPosition = new Vector3(0f, -1000f, 0f);
-        _rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutElastic);
-        _canvasGroup.DOFade(1, _fadeTime);
+        TooglePause();
+        _rectTransform.DOAnchorPos(new Vector2(0f, 0f), _fadeTime, false).SetEase(Ease.OutElastic).SetUpdate(true);
+        _canvasGroup.DOFade(1, _fadeTime).SetUpdate(true);
         
     }
 
     public void TooglePause()
     {
         _isPause = !_isPause;
+        Debug.Log(_isPause);
         Time.timeScale = _isPause ? 0 : 1;
     }
     public void PanelFadeOut()
@@ -28,8 +31,8 @@ public class UImanager : MonoBehaviour
         _canvasGroup.alpha = 1f;
         var sequence = DOTween.Sequence();
         _rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
-        sequence.Append(_rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutQuint));
-        sequence.Append(_canvasGroup.DOFade(0, _fadeTime));
-        
+        _rectTransform.DOAnchorPos(new Vector2(0f, -1000f), _fadeTime, false).SetEase(Ease.InOutQuint).SetUpdate(true);
+        sequence.Append(_canvasGroup.DOFade(0, _fadeTime).SetUpdate(true));
+        Invoke("TooglePause", _fadeTime);
     }
 }
