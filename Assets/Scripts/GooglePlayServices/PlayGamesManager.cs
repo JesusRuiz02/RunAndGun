a0c81f60e2f1ab972091c6c9c7a2ce328d9e8b40
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2024-07-16 13:44:11 -0700

    add sign in

---
+++ Assets/Scripts/GooglePlayServices/PlayGamesManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+
+public class PlayGamesManager : MonoBehaviour
+{
+    void Start()
+    {
+        SignIn();
+    }
+    public void SignIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+    
+    internal void ProcessAuthentication(SignInStatus status) {
+        if (status == SignInStatus.Success) {
+            // Continue with Play Games Services
+            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            string id = PlayGamesPlatform.Instance.GetUserId();
+            string imgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+            
+        } else {
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
+    }
+   
+}
